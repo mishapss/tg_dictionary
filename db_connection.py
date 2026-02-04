b78273b177576a947f7adf6cd06d181f1b596f6d
Misha <mishaps.leschenko@gmail.com>
2026-02-04 23:13:24 +0100

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,9 +1,9 @@
 import psycopg2
 
 connection = psycopg2.connect(
-    dbname="db_dictionary",
-    user="postgres",
-    password="PGS8!32_admin",
-    host="localhost",
-    port=5432
+    dbname="",
+    user="",
+    password="",
+    host="",
+    port=
 )
